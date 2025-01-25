afafd203e747fce7293bb3d0f984f4c567b9c328
tanzheen <85047594+tanzheen@users.noreply.github.com>
2025-01-26 00:27:03 +0800

    Update training.py

--- main/training.py
+++ main/training.py
@@ -1,219 +1,333 @@
-# *torch
-from pickletools import optimize
-# from sched import scheduler
+#!/usr/bin/env python
 import torch
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-from torch.optim import lr_scheduler as scheduler
-from torch.utils.data import DataLoader
-from torch.nn.utils.rnn import pad_sequence
 
+torch.backends.cudnn.deterministic = True
 
-# *user-defined
-
-import utils as utils
-
-# *basic
-import os
-import time
-import shutil
-import argparse, json, datetime
+import argparse
 import numpy as np
-from collections import OrderedDict
-from tqdm import tqdm
-import yaml
-import random
-import test as test
-import wandb
-import copy
-from pathlib import Path
-from typing import Iterable, Optional
-import math, sys
-from loguru import logger
+import os
+import shutil
+import time
+import queue
 
-from hpman.m import _
-import hpargparse
+from main.model import build_model
+from main.batch import Batch
+from main.helpers import (
+    log_data_info,
+    load_config,
+    log_cfg,
+    load_checkpoint,
+    make_model_dir,
+    make_logger,
+    set_seed,
+    symlink_update,
+)
+from main.model import SignModel
+from main.prediction import validate_on_data
+from main.loss import XentLoss
+from main.data import load_data, make_data_iter
+from main.builders import build_optimizer, build_scheduler, build_gradient_clipper
+from main.prediction import test
+from main.metrics import wer_single
+from main.vocabulary import SIL_TOKEN
+from torch import Tensor
+from torch.utils.tensorboard import SummaryWriter
+from torchtext.data import Dataset
+from typing import List, Dict
 
-# *metric
-from metrics import wer_list
-from sacrebleu.metrics import BLEU, CHRF, TER
-from data import load_data
-from model import build_model
 
-from torch.optim import Adam
-from torch.nn.utils.clip_grad import clip_grad_norm_
 
-class Trainer:
-    def __init__(self, model, train_dataloader, val_dataloader, config):
+# pylint: disable=too-many-instance-attributes
+class TrainManager:
+    """ Manages training loop, validations, learning rate scheduling
+    and early stopping."""
+
+    def __init__(self, model: SignModel, config: dict) -> None:
+        """
+        Creates a new TrainManager for a model, specified as in configuration.
+
+        :param model: torch module defining the model
+        :param config: dictionary containing the training configurations
+        """
+        train_config = config["training"]
+
+        # files for logging and storing
+        self.model_dir = make_model_dir(
+            train_config["model_dir"], overwrite=train_config.get("overwrite", False)
+        )
+        self.logger = make_logger(model_dir=self.model_dir)
+        self.logging_freq = train_config.get("logging_freq", 100)
+        self.valid_report_file = "{}/validations.txt".format(self.model_dir)
+        self.tb_writer = SummaryWriter(log_dir=self.model_dir + "/tensorboard/")
+
+        '''might need to change this feature_size and dataset_version'''
+        # input
+        self.feature_size = (
+            sum(config["data"]["feature_size"])
+            if isinstance(config["data"]["feature_size"], list)
+            else config["data"]["feature_size"]
+        )
+        self.dataset_version = config["data"].get("version", "phoenix_2014_trans")
+
+        # model
         self.model = model
-        self.train_dataloader = train_dataloader
-        self.val_dataloader = val_dataloader
-        self.config = config
-        
-        # Setup device
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = self.model.to(self.device)
-        
-        # Setup optimization
-        self.optimizer = Adam(
-            self.model.parameters(),
-            lr=config['training']['learning_rate'],
-            weight_decay=config['training']['weight_decay']
-        )
-        
-        # Learning rate scheduler
-        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            self.optimizer, 
-            mode='min',
-            factor=0.5,
-            patience=2,
-            verbose=True
-        )
-        
-        # Initialize tracking variables
-        self.best_val_loss = float('inf')
-        self.epochs_without_improvement = 0
-        
-    def train_epoch(self):
-        self.model.train()
-        total_loss = 0
-        
-        progress_bar = tqdm(self.train_dataloader, desc='Training')
-        for batch in progress_bar:
-            # Move batch to device
-            video = batch['video'].to(self.device)
-            attention_mask = batch['attention_mask'].to(self.device)
-            labels = batch['labels'].to(self.device)
-            
-            # Forward pass
-            self.optimizer.zero_grad()
+        self.txt_pad_index = self.model.txt_pad_index
+        self.txt_bos_index = self.model.txt_bos_index
+        self._log_parameters_list()
 
-            # this part might need to change 
-            outputs = self.model(
-                video=video,
-                attention_mask=attention_mask,
-                labels=labels
+        # we are defintely only doing translation
+        self.do_translation = (
+            config["training"].get("translation_loss_weight", 1.0) > 0.0
+        )
+
+        if self.do_translation:
+            self._get_translation_params(train_config=train_config)
+
+        # optimization
+        self.last_best_lr = train_config.get("learning_rate", -1)
+        self.learning_rate_min = train_config.get("learning_rate_min", 1.0e-8)
+        self.clip_grad_fun = build_gradient_clipper(config=train_config)
+
+        params = model.parameters()
+        self.optimizer = build_optimizer(
+            config=train_config, parameters=params
+        )
+        self.batch_multiplier = train_config.get("batch_multiplier", 1)
+
+         # validation & early stopping
+        self.validation_freq = train_config.get("validation_freq", 100)
+        self.num_valid_log = train_config.get("num_valid_log", 5)
+        self.ckpt_queue = queue.Queue(maxsize=train_config.get("keep_last_ckpts", 5))
+        self.eval_metric = train_config.get("eval_metric", "bleu")
+        if self.eval_metric not in ["bleu", "chrf", "wer", "rouge"]:
+            raise ValueError(
+                "Invalid setting for 'eval_metric': {}".format(self.eval_metric)
             )
-            
-            loss = outputs.loss
-            
-            # Backward pass
-            loss.backward()
-            clip_grad_norm_(self.model.parameters(), self.config['training']['max_grad_norm'])
-            self.optimizer.step()
-            
-            total_loss += loss.item()
-            progress_bar.set_postfix({'loss': loss.item()})
-            
-            if wandb.run:
-                wandb.log({'train_batch_loss': loss.item()})
-                
-        return total_loss / len(self.train_dataloader)
-    
-    def validate(self):
-        self.model.eval()
-        total_loss = 0
-        
-        with torch.no_grad():
-            for batch in tqdm(self.val_dataloader, desc='Validating'):
-                video = batch['video'].to(self.device)
-                attention_mask = batch['attention_mask'].to(self.device)
-                labels = batch['labels'].to(self.device)
-                
-                outputs = self.model(
-                    video=video,
-                    attention_mask=attention_mask,
-                    labels=labels
-                )
-                
-                loss = outputs.loss
-                total_loss += loss.item()
-                
-        return total_loss / len(self.val_dataloader)
-    
-    def train(self):
-        for epoch in range(self.config['training']['num_epochs']):
-            print(f"\nEpoch {epoch+1}/{self.config['training']['num_epochs']}")
-            
-            train_loss = self.train_epoch()
-            val_loss = self.validate()
-            
-            # Log metrics
-            print(f"Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f}")
-            if wandb.run:
-                wandb.log({
-                    'train_loss': train_loss,
-                    'val_loss': val_loss,
-                    'epoch': epoch
-                })
-            
-            # Learning rate scheduling
-            self.scheduler.step(val_loss)
-            
-            # Save best model
-            if val_loss < self.best_val_loss:
-                self.best_val_loss = val_loss
-                self.epochs_without_improvement = 0
-                self.save_checkpoint(epoch, val_loss)
-            else:
-                self.epochs_without_improvement += 1
-            
-            # Early stopping
-            if self.epochs_without_improvement >= self.config['training']['early_stopping_patience']:
-                print("Early stopping triggered")
-                break
-    
-    def save_checkpoint(self, epoch, val_loss):
-        checkpoint = {
-            'epoch': epoch,
-            'model_state_dict': self.model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict(),
-            'val_loss': val_loss,
-        }
-        
-        checkpoint_path = os.path.join(
-            self.config['training']['checkpoint_dir'],
-            f'model_epoch_{epoch}_loss_{val_loss:.4f}.pt'
+        self.early_stopping_metric = train_config.get(
+            "early_stopping_metric", "eval_metric"
         )
+
+        # if we schedule after BLEU/chrf, we want to maximize it, else minimize
+        # early_stopping_metric decides on how to find the early stopping point:
+        # ckpts are written when there's a new high/low score for this metric
+        if self.early_stopping_metric in [
+            "ppl",
+            "translation_loss",
+            "recognition_loss",
+        ]:
+            self.minimize_metric = True
+        elif self.early_stopping_metric == "eval_metric":
+            if self.eval_metric in ["bleu", "chrf", "rouge"]:
+                assert self.do_translation
+                self.minimize_metric = False
+            else:  # eval metric that has to get minimized (not yet implemented)
+                self.minimize_metric = True
+        else:
+            raise ValueError(
+                "Invalid setting for 'early_stopping_metric': {}".format(
+                    self.early_stopping_metric
+                )
+            )
         
-        torch.save(checkpoint, checkpoint_path)
-        print(f"Saved checkpoint: {checkpoint_path}")
+        # learning rate scheduling
+        self.scheduler, self.scheduler_step_at = build_scheduler(
+            config=train_config,
+            scheduler_mode="min" if self.minimize_metric else "max",
+            optimizer=self.optimizer,
+            hidden_size=config["model"]["encoder"]["hidden_size"],
+        )
 
-def main(args, config):
-    # Set device
-    device = torch.device(args.device)
-    
-    # Set random seed
-    seed = args.seed
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    
-    # Load data
-    train_data, dev_data, test_data, txt_vocab, txt_field = load_data(config["data"], args)
-    
-    # Build model
-    model = build_model(
-        cfg=config["model"],
-        sgn_dim=512,
-        txt_vocab=txt_vocab,
-        multimodal=False,
-        do_translation=True
-    )
-    
-    # Create trainer
-    trainer = Trainer(
-        model=model,
-        train_dataloader=train_data,
-        val_dataloader=dev_data,
-        config=config
-    )
-    
-    # Train
-    trainer.train()
+        # data & batch handling
+        self.level = config["data"]["level"]
+        if self.level not in ["word", "bpe", "char"]:
+            raise ValueError("Invalid segmentation level': {}".format(self.level))
+        
+        self.shuffle = train_config.get("shuffle", True)
+        self.epochs = train_config["epochs"]
+        self.batch_size = train_config["batch_size"]
+        self.batch_type = train_config.get("batch_type", "sentence")
+        self.eval_batch_size = train_config.get("eval_batch_size", self.batch_size)
+        self.eval_batch_type = train_config.get("eval_batch_type", self.batch_type)
+
+        self.use_cuda = train_config["use_cuda"]
+        if self.use_cuda:
+            self.model.cuda()
+            if self.do_translation:
+                self.translation_loss_function.cuda()
+            if self.do_recognition:
+                self.recognition_loss_function.cuda()
+
+        # initialize training statistics
+        self.steps = 0
+        # stop training if this flag is True by reaching learning rate minimum
+        self.stop = False
+        self.total_txt_tokens = 0
+        self.total_gls_tokens = 0
+        self.best_ckpt_iteration = 0
+        # initial values for best scores
+        self.best_ckpt_score = np.inf if self.minimize_metric else -np.inf
+        self.best_all_ckpt_scores = {}
+        # comparison function for scores
+        self.is_best = (
+            lambda score: score < self.best_ckpt_score
+            if self.minimize_metric
+            else score > self.best_ckpt_score
+        )
+
+        # model parameters
+        if "load_model" in train_config.keys():
+            model_load_path = train_config["load_model"]
+            self.logger.info("Loading model from %s", model_load_path)
+            reset_best_ckpt = train_config.get("reset_best_ckpt", False)
+            reset_scheduler = train_config.get("reset_scheduler", False)
+            reset_optimizer = train_config.get("reset_optimizer", False)
+            self.init_from_checkpoint(
+                model_load_path,
+                reset_best_ckpt=reset_best_ckpt,
+                reset_scheduler=reset_scheduler,
+                reset_optimizer=reset_optimizer,
+            )
+
+
+        def _get_translation_params(self, train_config) -> None:
+        self.label_smoothing = train_config.get("label_smoothing", 0.0)
+        self.translation_loss_function = XentLoss(
+            pad_index=self.txt_pad_index, smoothing=self.label_smoothing
+        )
+        self.translation_normalization_mode = train_config.get(
+            "translation_normalization", "batch"
+        )
+        if self.translation_normalization_mode not in ["batch", "tokens"]:
+            raise ValueError(
+                "Invalid normalization {}.".format(self.translation_normalization_mode)
+            )
+        self.translation_loss_weight = train_config.get("translation_loss_weight", 1.0)
+        self.eval_translation_beam_size = train_config.get(
+            "eval_translation_beam_size", 1
+        )
+        self.eval_translation_beam_alpha = train_config.get(
+            "eval_translation_beam_alpha", -1
+        )
+        self.translation_max_output_length = train_config.get(
+            "translation_max_output_length", None
+        )
+
+    def _save_checkpoint(self) -> None:
+        """
+        Save the model's current parameters and the training state to a
+        checkpoint.
+
+        The training state contains the total number of training steps,
+        the total number of training tokens,
+        the best checkpoint score and iteration so far,
+        and optimizer and scheduler states.
+
+        """
+        model_path = "{}/{}.ckpt".format(self.model_dir, self.steps)
+        state = {
+            "steps": self.steps,
+            "total_txt_tokens": self.total_txt_tokens if self.do_translation else 0,
+            "total_gls_tokens": self.total_gls_tokens if self.do_recognition else 0,
+            "best_ckpt_score": self.best_ckpt_score,
+            "best_all_ckpt_scores": self.best_all_ckpt_scores,
+            "best_ckpt_iteration": self.best_ckpt_iteration,
+            "model_state": self.model.state_dict(),
+            "optimizer_state": self.optimizer.state_dict(),
+            "scheduler_state": self.scheduler.state_dict()
+            if self.scheduler is not None
+            else None,
+        }
+        torch.save(state, model_path)
+        if self.ckpt_queue.full():
+            to_delete = self.ckpt_queue.get()  # delete oldest ckpt
+            try:
+                os.remove(to_delete)
+            except FileNotFoundError:
+                self.logger.warning(
+                    "Wanted to delete old checkpoint %s but " "file does not exist.",
+                    to_delete,
+                )
+
+        self.ckpt_queue.put(model_path)
+
+        # create/modify symbolic link for best checkpoint
+        symlink_update(
+            "{}.ckpt".format(self.steps), "{}/best.ckpt".format(self.model_dir)
+        )
+
+    def init_from_checkpoint(
+        self,
+        path: str,
+        reset_best_ckpt: bool = False,
+        reset_scheduler: bool = False,
+        reset_optimizer: bool = False,
+    ) -> None:
+        """
+        Initialize the trainer from a given checkpoint file.
+
+        This checkpoint file contains not only model parameters, but also
+        scheduler and optimizer states, see `self._save_checkpoint`.
+
+        :param path: path to checkpoint
+        :param reset_best_ckpt: reset tracking of the best checkpoint,
+                                use for domain adaptation with a new dev
+                                set or when using a new metric for fine-tuning.
+        :param reset_scheduler: reset the learning rate scheduler, and do not
+                                use the one stored in the checkpoint.
+        :param reset_optimizer: reset the optimizer, and do not use the one
+                                stored in the checkpoint.
+        """
+        model_checkpoint = load_checkpoint(path=path, use_cuda=self.use_cuda)
+
+        # restore model and optimizer parameters
+        self.model.load_state_dict(model_checkpoint["model_state"])
+
+        if not reset_optimizer:
+            self.optimizer.load_state_dict(model_checkpoint["optimizer_state"])
+        else:
+            self.logger.info("Reset optimizer.")
+
+        if not reset_scheduler:
+            if (
+                model_checkpoint["scheduler_state"] is not None
+                and self.scheduler is not None
+            ):
+                self.scheduler.load_state_dict(model_checkpoint["scheduler_state"])
+        else:
+            self.logger.info("Reset scheduler.")
+
+        # restore counts
+        self.steps = model_checkpoint["steps"]
+        self.total_txt_tokens = model_checkpoint["total_txt_tokens"]
+        self.total_gls_tokens = model_checkpoint["total_gls_tokens"]
+
+        if not reset_best_ckpt:
+            self.best_ckpt_score = model_checkpoint["best_ckpt_score"]
+            self.best_all_ckpt_scores = model_checkpoint["best_all_ckpt_scores"]
+            self.best_ckpt_iteration = model_checkpoint["best_ckpt_iteration"]
+        else:
+            self.logger.info("Reset tracking of the best checkpoint.")
+
+        # move parameters to cuda
+        if self.use_cuda:
+            self.model.cuda()
+
+    def _log_parameters_list(self) -> None:
+        """
+        Write all model parameters (name, shape) to the log.
+        """
+        model_parameters = filter(lambda p: p.requires_grad, self.model.parameters())
+        n_params = sum([np.prod(p.size()) for p in model_parameters])
+        self.logger.info(f"Total params: {n_params:,}")
+        trainable_params = [
+            n for (n, p) in self.model.named_parameters() if p.requires_grad
+        ]
+        self.logger.info("Trainable parameters: %s", sorted(trainable_params))
+        assert trainable_params
+        
 
 
 
 
-
+        
 
